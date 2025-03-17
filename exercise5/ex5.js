8af3513817b43b48b34532c543180766c599ca94
silvanacanessa <silvanacanessa@icloud.com>
2025-03-17 15:25:50 -0400

    d

---
+++ exercise5/ex5.js
@@ -0,0 +1,6 @@
+const first = document.querySelectorAll('#first');
+const second = document.querySelectorAll('#second');
+const third = document.querySelectorAll('#third');
+const fourth = document.querySelectorAll('#fourth');
+const fifth = document.querySelectorAll('#fifth');
+
